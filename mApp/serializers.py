563e0c3e71a711a68dc6e00f13a89aed3e5a2bd0
Masih <bahmani.masih@gmail.com>
2021-04-10 20:10:45 +0430

    Clean-up Serializers.py codes

--- mApp/serializers.py
+++ mApp/serializers.py
@@ -26,13 +26,14 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username','password' ,'email', 'first_name', 'last_name', 'phone_number',
+        fields = ('id', 'username', 'password', 'email', 'first_name', 'last_name', 'phone_number',
                   'profile_image', 'university', 'field_of_study', 'entry_year')
-        extra_kwargs = {'paswword': {'write_only': True}}
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
 
 class LoginSerializer(serializers.Serializer):
 
