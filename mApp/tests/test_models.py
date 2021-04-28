632cd2c63dc45ec64c83df328198312cfe7a62a0
Masih <bahmani.masih@gmail.com>
2021-04-28 11:38:49 +0430

    Implement bid model test

--- mApp/tests/test_models.py
+++ mApp/tests/test_models.py
@@ -1,11 +1,32 @@
 from django.test import TestCase
-from ..models import Post
+from ..models import User, Post, Bid
 
 
-class PostTest(TestCase):
+class TestModels(TestCase):
     """ Test module for Post model """
 
     def setUp(self):
+        User.objects.create(username='masih@gmail.com',
+                            email='masih@gmail.com',
+                            first_name='masih',
+                            last_name='bn',
+                            password='123456',
+                            phone_number=9125557558,
+                            university='iust',
+                            field_of_study='ce',
+                            entry_year=97)
+
+        Post.objects.create(title='riazi 1 faramarzi',
+                            author='faramarzi',
+                            publisher='gaj',
+                            categories='math$riazi',
+                            price=10000,
+                            province='tehran',
+                            zone='narmak',
+                            status='sell',
+                            description='some description',
+                            is_active=True)
+
         Post.objects.create(
             title='SE modern approach', categories=3, city='tehran')
         Post.objects.create(
@@ -18,3 +39,12 @@ class PostTest(TestCase):
             p1.get_city(), "SE modern approach is in tehran city.")
         self.assertEqual(
             p2.get_city(), "Ditel and Ditel is in esfahan city.")
+
+    def test_bid_owner_and_post(self):
+        b1 = Bid.objects.create(post=Post.objects.get(id=1),
+                                owner=User.objects.get(id=1),
+                                offered_price=9000,
+                                description='some random description')
+
+        self.assertEqual(b1.owner, User.objects.get(id=1))
+        self.assertEqual(b1.post, Post.objects.get(id=1))
